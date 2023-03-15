251e6fc05935b9dd292030a3ca567b3c20525da3
StevenA1402 <gonzalezelvert@gmail.com>
2023-03-15 13:46:36 -0500

    Juego

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let completar = document.getElementById("Completar");
+//Declarar variables
 let letraRepetida = [];
+let completar = document.getElementById("Completar");
 const palabras = [
   "queso",
   "gato",
@@ -12,23 +13,23 @@ const palabras = [
   "correr",
   "ñero",
 ];
+let letra = document.getElementById("letra");
 let palabra = palabras[Math.floor(Math.random() * palabras.length)];
 let guiones = palabra.replace(/./g, "_ ");
 let boton = document.getElementById("boton");
-let historial = document.getElementById("lista")
+let historial = document.getElementById("lista");
 let fallaste = true;
+//Funciones:
+//Funcion boton inicio
 boton.addEventListener("click", (e) => {
   completar.innerHTML = `            <div id="palabrasGuiones" hidden class=" flex items-center justify-center pb-[6rem] text-white font-bold text-[4rem]"  >
     ${guiones}</div>
-     <input   maxlength="1"  id="letra" class="bg-white" type="text">
+    <input   maxlength="1"  id="letra" class="bg-white" type="text">
      <button id="boton2"   class=" w-[15rem] h-[4rem] hover:bg-white hover:text-black text-[2rem] font-bold bg-lime-500	  rounded-md" >Comprobar</button>
      
     `;
 
-  letra = document.getElementById('letra')
-  letra.addEventListener("input", () => {
-
-  });
+  //Declaro variables para uso dentro del boton
   let contador = 0;
   let palabrasGuiones = document.getElementById("palabrasGuiones");
   let getLetter = document.getElementById("letra");
@@ -39,8 +40,7 @@ boton.addEventListener("click", (e) => {
   reinicio.hidden = false;
   boton.hidden = true;
 
-
-
+  // Pinta el ahorcado
   const contadorFallos = (fallaste) => {
     if (fallaste) {
       contador++;
@@ -55,15 +55,14 @@ boton.addEventListener("click", (e) => {
         perdiste.innerHTML = `<p class="text-lime-400">VICTORIA </p>`;
       }
     }
-  }
-
+  };
+  //pinta las letras
   const fallo = (fallaste, letra) => {
     for (let i in palabra) {
       if (letra == palabra[i]) {
         guiones =
           guiones.substring(0, i * 2) + letra + guiones.substring(i * 2 + 1);
         fallaste = false;
-
       }
     }
 
@@ -74,35 +73,46 @@ boton.addEventListener("click", (e) => {
       }
     }
 
-    contadorFallos(fallaste)
+    contadorFallos(fallaste);
   };
+  
 
+  //Funcion letra repetida 
+
+  const LetraRepetida = (e) => {
+    let 
+  }
+
+  // Funcion -Boton de comprobar
   boton2.addEventListener("click", () => {
     letra = getLetter.value;
-    fallo(fallaste, letra)
-
-    historial.innerHTML += `<p class="font-bold text-[1.5rem] text-white"> Has usado la letra: ${letra}</p>`
-    letraRepetida.push(guiones);
-    getLetter.value = "";
-    getLetter.focus();
-
+    if (letra == "") {
+      historial.innerHTML += `<p class="font-bold text-[1.5rem] text-white"> Ingrese una letra o palabra</p>`;
+    } else {
+      letraRepetida.push(guiones);
+      fallo(fallaste, letra);
+      historial.innerHTML += `<p class="font-bold text-[1.5rem] text-white"> Has usado la letra: ${letra}</p>`;
+      palabrasGuiones.innerHTML = `${guiones}`;
+      getLetter.value = "";
+      getLetter.focus();
+    }
   });
-
+  //Funcion - boton de reinicio
   reinicio.addEventListener("click", () => {
     window.location.reload();
   });
 
-
+  //Funcion similar al boton comprobar
   getLetter.addEventListener("keypress", (e) => {
-    if (e.keyCode == 13) {
-      historial.innerHTML +=`<p class="font-bold text-[1.5rem] text-white"> Has usado la letra: ${getLetter.value}</p>`
+    letra = getLetter.value;
+    enter = e.keyCode == 13;
+    if (enter) {
+      historial.innerHTML += `<p class="font-bold text-[1.5rem] text-white"> Has usado la letra: ${getLetter.value}</p>`;
       letraRepetida.push(guiones);
-      fallo(fallaste, getLetter.value)
+      fallo(fallaste, getLetter.value);
       getLetter.value = "";
       getLetter.focus();
-      palabrasGuiones.innerHTML = `${guiones}`; 
+      palabrasGuiones.innerHTML = `${guiones}`;
     }
-
   });
-
 });
